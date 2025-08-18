9a9b19dcd705ab56b342617814b19a03fc34efea
VarunGupta2005 <varun03072005@gmail.com>
2025-08-19 02:05:59 +0530

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from my AUTOMATICALLY deployed Node.js app!');
+  res.send('Hello from my AUTOMATICALLY deployed Node.js app yay!');
 });
 
 app.listen(PORT, () => console.log(`Node app listening on port ${PORT}`));
