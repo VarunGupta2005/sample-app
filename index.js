8434272f4a55e2017d5abb178d2c947f31a3d628
VarunGupta2005 <varun03072005@gmail.com>
2025-08-21 15:23:37 +0530

    final

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('good morninh');
+  res.send('good mornin');
 });
 
 app.listen(PORT, () => console.log(`Nodde apffffp listening on port ${PORT}`));   
