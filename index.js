96f1ed7b152d1ff322d94467dee8670f144e7621
VarunGupta2005 <varun03072005@gmail.com>
2025-08-24 18:38:47 +0530

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('good morninng frns');
+  res.send('good morninng frns hello');
 });
 
 app.listen(PORT, () => console.log(`Node app listening on port ${PORT}`));
