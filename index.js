c94342e7d58c24cca3bd7fbbf666c4569ea7f880
VarunGupta2005 <varun03072005@gmail.com>
2025-08-25 13:09:05 +0530

    fix railway CLI command - use --service flag directly

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('really daaddyS!!!!!!!!!!!!!!!|||');
+  res.send('really daaddyS!!!!!!!!!!!!!|||');
 });
 
 app.listen(PORT, () => console.log(`Node app listening on port ${PORT}`));
